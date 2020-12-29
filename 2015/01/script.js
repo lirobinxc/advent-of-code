42276121e32f76c207a19a4e50ae3a47461df901
Robin Li <lirobinxc@gmail.com>
2020-12-28 21:25:57 -0500

    solved

---
+++ 2015/01/script.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+
+const file = fs.readFileSync('./2015/01/input.txt', (err, data) => {
+  if (err) console.error('Read file error');
+})
+
+const input = file.toString();
+
+// Part 1
+function question1() {
+  let currentFloor = 0;
+
+  for (let i of input) {
+    if (i === '(') {
+      currentFloor++
+    } else currentFloor--;
+  }
+
+  console.log('Part 1 Answer:', currentFloor)
+}
+
+// Part 2
+function question2() {
+  const arr = input.split('')
+  let currentFloor = 0;
+  let answer = arr.map(i => {
+    return i === '('
+    ? ++currentFloor
+    : --currentFloor;
+  })
+  console.log('Part 2:', answer.indexOf(-1) + 1)
+}
+question2();
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# advent-of-code
+## Advent of Code - Solutions
+
+All puzzles solved using Javascript in NodeJS.
+Each solution is timed.
+
+#### 2015
+1. **Part 1:** 4.156 ms  -  **Part 2:** 
